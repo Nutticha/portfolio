c027e53a72fd2c0423474591fa45f1b2cc400747
Nutticha <nutticha30234@gmail.com>
2022-05-25 21:39:58 +0700

    up

--- src/assets/data/shape.data.js
+++ src/assets/data/shape.data.js
@@ -2,37 +2,37 @@ const SHAPE = [
     {
         id:1,
         classname:'shape-one',
-        images:'https://i3.fpic.cc/file/img-b1/2022/01/18/011b313d0b6c14f6ce.png',
+        images: '../image/p1.png',
         alt:'images'
     },
     {
         id:2,
         classname:'shape-two',
-        images:'https://i3.fpic.cc/file/img-b1/2022/01/18/02.png',
+        images:'../image/p2.png',
         alt:'images'
     },
     {
         id:3,
         classname:'shape-three',
-        images:'https://i3.fpic.cc/file/img-b1/2022/01/18/036899df67953a9f81.png',
+        images:'../image/p3.png',
         alt:'images'
     },
     {
         id:4,
         classname:'shape-four',
-        images:'https://i3.fpic.cc/file/img-b1/2022/01/18/04.png',
+        images:'../image/p4.png',
         alt:'images'
     },
     {
         id:5,
         classname:'shape-five',
-        images:'https://i3.fpic.cc/file/img-b1/2022/01/18/05.png',
+        images:'../image/p5.png',
         alt:'images'
     },
     {
         id:6,
         classname:'shape-six',
-        images:'https://i3.fpic.cc/file/img-b1/2022/01/18/06.png',
+        images:'../image/p6.png',
         alt:'images'
     },
 ]
