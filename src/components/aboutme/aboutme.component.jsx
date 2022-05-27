a90e3658955e4f5ae0c2fb21208d9bd3972aeba0
Nutticha <nutticha30234@gmail.com>
2022-05-27 21:00:18 +0700

    up

--- src/components/aboutme/aboutme.component.jsx
+++ src/components/aboutme/aboutme.component.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
 
             <div className="description">
                 <p>สวัสดีคะ ดิชั้นชื่อ นางสาวณัฐธิชา ศรีเกิด ชื่อเล่น มายด์ อายุ 21 ปี มาจากมหาวิทยาลัยพระจอมเกล้าพระนครเหนือ วิทยาเขตปราจีนบุรี คณะเทคโนโลยีและการจักการอุตสาหกรรม สาขาเทคโนโลยีสารสนเทศ ปีที่ 4 </p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut reprehenderit possimus laborum, modi quasi consectetur voluptate ad iusto, magni minima, autem explicabo obcaecati alias! Eligendi quam praesentium omnis quo eaque?</p>
+                <p>ความถนัด ipsum dolor sit amet consectetur adipisicing elit. Ut reprehenderit possimus laborum, modi quasi consectetur voluptate ad iusto, magni minima, autem explicabo obcaecati alias! Eligendi quam praesentium omnis quo eaque?</p>
             </div>
 
             <div className="email position-relative">
