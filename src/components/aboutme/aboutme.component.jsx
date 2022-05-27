d837ccc528be97f01f1a2b6aa84d1c75d4082e24
Nutticha <nutticha30234@gmail.com>
2022-05-27 21:16:03 +0700

    up

--- src/components/aboutme/aboutme.component.jsx
+++ src/components/aboutme/aboutme.component.jsx
@@ -4,11 +4,12 @@ import SectionTop from '../sectiontop/sectiontop.component';
 const AboutMe = () => {
     return (
         <div className='col-lg-7'>
-            <SectionTop Title="สวัสดีจ้าทุกคน" subTitle="เกี่ยวกับฉัน"/>
+            <SectionTop Title="สวัสดีจ้า ทุกคน" subTitle="เกี่ยวกับฉัน"/>
 
             <div className="description">
                 <p>สวัสดีคะ ดิชั้นชื่อ นางสาวณัฐธิชา ศรีเกิด ชื่อเล่น มายด์ อายุ 21 ปี มาจากมหาวิทยาลัยพระจอมเกล้าพระนครเหนือ วิทยาเขตปราจีนบุรี คณะเทคโนโลยีและการจักการอุตสาหกรรม สาขาเทคโนโลยีสารสนเทศ ปีที่ 4 </p>
-                <p>ความถนัด ipsum dolor sit amet consectetur adipisicing elit. Ut reprehenderit possimus laborum, modi quasi consectetur voluptate ad iusto, magni minima, autem explicabo obcaecati alias! Eligendi quam praesentium omnis quo eaque?</p>
+                <SectionTop subTitle = "ข้อมูลส่วนตัว"></SectionTop>
+                <p></p>
             </div>
 
             <div className="email position-relative">
